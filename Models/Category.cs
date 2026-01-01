46f6f3ef55cd63eded97011ceac55874994b554d
Gan Muhammad Rizqan <ganmrizqan@gmail.com>
2026-01-01 21:49:56 +0700

    Create models

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Neksara.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsDeleted { get; set; } = false;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public int ViewCount { get; set; } = 0;
+
+        
+        public ICollection<Topic> Topics { get; set; } = new HashSet<Topic>();
+        public ICollection<Feedback> Feedbacks { get; set; } = new HashSet<Feedback>();
+    }
+}
